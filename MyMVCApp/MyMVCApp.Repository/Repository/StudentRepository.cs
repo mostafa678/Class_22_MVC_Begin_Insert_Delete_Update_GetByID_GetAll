8c8d28f9c589522f39bd5554f8c7f8916854b29e
Mostafa678 <turjo2222@gmail.com>
2019-07-21 04:01:59 +0600

    MVC_Begin_Insert_Delete_Update_GetByID_GetAll

---
+++ MyMVCApp/MyMVCApp.Repository/Repository/StudentRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMVCApp.Models.Models;
+using MyMVCApp.DatabaseContext.DatabaseContext;
+using System.Data.Entity;
+
+namespace MyMVCApp.Repository.Repository
+{
+    public class StudentRepository
+    {
+        StudentDBContext db = new StudentDBContext();
+        public bool Add(Student student)
+        {
+            int isExecuted = 0;
+            db.Students.Add(student);
+            isExecuted = db.SaveChanges();
+            if (isExecuted > 0)
+                return true;
+            return false;
+        }
+        public bool Delete(Student student)
+        {
+            int isExecuted = 0;
+            Student aStudent = db.Students.FirstOrDefault(c => c.ID == student.ID);
+            if (aStudent != null)
+            {
+                db.Students.Remove(aStudent);
+                isExecuted = db.SaveChanges();
+            }
+
+            if (isExecuted > 0)
+                return true;
+            return false;
+        }
+        public bool Update(Student student)
+        {
+            int isExecuted = 0;
+            db.Entry(student).State = EntityState.Modified;
+            isExecuted = db.SaveChanges();
+            if (isExecuted > 0)
+                return true;
+            return false;
+        }
+        public List<Student> GetAll()
+        {
+            
+            return db.Students.ToList();
+        }
+        public Student GetByID(Student student)
+        {
+            Student aStudent = db.Students.FirstOrDefault(c => c.ID == student.ID);
+            return aStudent;
+        }
+    }
+}
---
+++ MyMVCApp/MyMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApp.BLL.BLL;
+using MyMVCApp.Models.Models;
+
+namespace MyMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentManager _studentManager =new StudentManager();
+        private Student _student = new Student();
+        
+        // GET: Student
+        public ActionResult Add()
+        {
+            _student.ID = 101;
+            _student.Name = "Ali";
+            _studentManager.Add(_student);
+
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            _student.ID = 5;
+            
+            _studentManager.Delete(_student);
+
+            return View();
+        }
+        public ActionResult Update()
+        {
+            _student.ID = 5;
+            Student aStudent = _studentManager.GetByID(_student);
+            if (aStudent != null)
+            {
+                aStudent.Name = "Azad";
+                _studentManager.Update(aStudent);
+            }
+
+            return View();
+
+            
+        }
+
+        public ActionResult GetByID()
+        {
+           // _student.ID = 2;
+
+            _studentManager.GetByID(_student);
+
+            return View();
+        }
+        public ActionResult GetAll()
+        {
+
+
+            List<Student> bStudent = _studentManager.GetAll();
+
+            return View();
+        }
+    }
+}
